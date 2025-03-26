02b50a750483d5a0888239a3bee69a97f960218a
vrymel <vrymel.dev@gmail.com>
2025-03-26 21:34:05 +0800

    Added Ride model

--- core/models.py
+++ core/models.py
@@ -14,3 +14,23 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Ride(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'New'),
+        ('en-route', 'En-Route'),
+        ('pickup', 'Pickup'),
+        ('dropoff', 'Dropoff'),
+    ]
+
+    id_ride = models.AutoField(primary_key=True)
+    status = models.CharField(max_length=255, choices=STATUS_CHOICES, default='en-route')
+    id_rider = models.ForeignKey(User, on_delete=models.PROTECT, related_name='rider')
+    id_driver = models.ForeignKey(User, on_delete=models.PROTECT, related_name='driver')
+    pickup_latitude = models.DecimalField(decimal_places=7, max_digits=12, null=True)
+    pickup_longitude = models.DecimalField(decimal_places=7, max_digits=12, null=True)
+    dropoff_latitude = models.DecimalField(decimal_places=7, max_digits=12, null=True)
+    dropoff_longitude = models.DecimalField(decimal_places=7, max_digits=12, null=True)
+    pickup_time = models.DateTimeField()
+
