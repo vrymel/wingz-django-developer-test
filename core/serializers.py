9bcc7600b837aa21dfb1a42f9428596721c56ef2
vrymel <vrymel.dev@gmail.com>
2025-03-27 14:09:46 +0800

    Renamed ride_events to todays_ride_events and limit it to data from the last 24 hours only

--- core/serializers.py
+++ core/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from datetime import timedelta
+from django.utils.timezone import now
 from core.models import User, Ride, RideEvent
 
 
@@ -18,7 +20,7 @@ class RideSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ride
         fields = ['id_ride', 'id_rider', 'rider', 'id_driver', 'driver', 'status', 'pickup_latitude',
-                  'pickup_longitude', 'dropoff_latitude', 'dropoff_longitude', 'pickup_time', 'ride_events', 'distance']
+                  'pickup_longitude', 'dropoff_latitude', 'dropoff_longitude', 'pickup_time', 'todays_ride_events', 'distance']
 
     rider = UserSerializer(source='id_rider', read_only=True)
     id_rider = serializers.PrimaryKeyRelatedField(queryset=User.objects, write_only=True)
@@ -26,7 +28,7 @@ class RideSerializer(serializers.ModelSerializer):
     driver = UserSerializer(source='id_driver', read_only=True)
     id_driver = serializers.PrimaryKeyRelatedField(queryset=User.objects, write_only=True)
 
-    ride_events = RideEventSerializer(many=True, read_only=True)
+    todays_ride_events = serializers.SerializerMethodField()
 
     distance = serializers.SerializerMethodField()
 
@@ -36,3 +38,12 @@ class RideSerializer(serializers.ModelSerializer):
             return obj.distance
 
         return None
+
+    @staticmethod
+    def get_todays_ride_events(obj):
+        # Filter ride events to include only those from the last 24 hours
+        last_24_hours = now() - timedelta(hours=24)
+        events = RideEvent.objects.filter(id_ride=obj.id_ride, created_at__gte=last_24_hours)
+        
+        return RideEventSerializer(events, many=True).data
+    
