7417c8f728f72bb22b64aca0520abdcf8306c106
vrymel <vrymel.dev@gmail.com>
2025-03-28 10:42:03 +0800

    Added STATIC_ROOT

--- wingz/settings.py
+++ wingz/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
+STATIC_ROOT = str(ROOT_DIR / "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
